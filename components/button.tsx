3c5de85dbf1fead9a05e1f0ddf09fec0e604e4ae
akins-dev <bobemmanuel503@gmail.com>
2023-09-20 19:40:25 +0100

    chore: made navbar responsive

--- components/button.tsx
+++ components/button.tsx
@@ -8,8 +8,8 @@ const Button = ({
     content
 }: ButtonProp) => {
   return (
-    <button className="bg-gradient-to-r from-gradientx to-gradienty rounded h-[52px] w-[172px] items-center">
-        <p className="text-[16px] text-center">{content}</p>
+    <button className="bg-gradient-to-r from-gradientx to-gradienty rounded lg:h-[52px] md:h-[36px] lg:w-[172px] md:w-[120px] items-center">
+        <p className="lg:text-[16px] md:text-sm text-center">{content}</p>
     </button>
   )
 }
--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -6,22 +6,22 @@ import Button from "@/components/button"
 
 const Navbar = () => {
   return (
-    <div className="flex mt-[50px] mb-[20px] mx-[100px] justify-between items-center">
-      <div className="text-[36px] font-bold">
+    <div className="flex lg:mt-[50px] mt-[20px] lg:mb-[20px] mb-[10px] lg:mx-[100px] mx-[30px] justify-between items-center overflow-hidden">
+      <div className="lg:text-[36px] md:text-[24px] sm:text-[15px] font-bold">
         <Link href="/">
           get<span className="text-cpink">linked</span>
         </Link>
       </div>
 
-      <div className="flex items-center gap-x-14">
-        <div className="flex items-center gap-x-10">
-          <div className="flex font-semibold cursor-pointer">
+      <div className="hidden items-center gap-x-14 md:flex">
+        <div className="flex items-center md:gap-x-4 lg:gap-x-10">
+          <div className="flex font-semibold lg:text-[16px] md:text-sm cursor-pointer">
             Timeline
           </div>
-          <div className="flex font-semibold cursor-pointer">
+          <div className="flex font-semibold lg:text-[16px] md:text-sm cursor-pointer">
             Overview
           </div>
-          <div className="flex font-semibold cursor-pointer">
+          <div className="flex font-semibold lg:text-[16px] md:text-sm cursor-pointer">
             FAQ
           </div>
           <div className="flex cursor-pointer">
