79904e8e2340a6f8530b0754e449fa06d632c8e9
akins-dev <bobemmanuel503@gmail.com>
2023-09-20 03:50:16 +0100

    chore: setup folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+
+const montsrerat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'getlinked - Register',
+  description: 'Igniting a revolution in HR vision',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={montsrerat.className}>{children}</body>
+    </html>
+  )
+}
